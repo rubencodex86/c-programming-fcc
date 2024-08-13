50f4f8adb9d65f0e9dbcc0708010247b2ef8a6b5
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 19:50:50 +0000

    Console Inputs and Outputs

---
+++ ConsoleInOut/ConsoleInOut/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConsoleInOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello my name is Aba!");
+            
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            //Console.WriteLine(name);
+            
+            Console.Write("Enter your age: ");
+            //--------------------------------------------
+            // string ageInput = Console.ReadLine();
+            // int age = Convert.ToInt32(ageInput);
+            
+            //or 
+
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            //--------------------------------------------
+            
+            //Console.WriteLine(age);
+            
+            // Console.Write("Your name is ");
+            // Console.Write(name);
+            // Console.Write(" and your age is ");
+            // Console.Write(age);
+
+            Console.WriteLine();
+            
+            //Concatenate text
+            Console.WriteLine("Your name is " + name + " and your age is " + age);
+        }
+    }
+}
