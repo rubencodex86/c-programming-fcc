2c4918569b0c9a38ec2916bf1c1c8b0b5765caf8
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 19:25:00 +0000

    Exercise Two - Odd Even Checker - Solved

---
+++ ExerciseOddEvenChecker/ExerciseOddEvenChecker/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseOddEvenChecker
+{
+    class Program
+    {
+        /*
+         * Create & initialise two ints
+         * Make a variable to work out the remainder
+         * Output remainder to the screen
+         * Change the first int variable to another number
+            * and recalculate the remainder
+            * output new remainder to the screen
+         */
+        static void Main(string[] args)
+        {
+            int numOne = 10;
+            int numTwo = 2;
+            var remainder = numOne % numTwo;
+            Console.WriteLine(remainder);
+
+            numOne = 101;
+            remainder = numOne % numTwo;
+            Console.WriteLine(remainder);
+        }
+    }
+}
