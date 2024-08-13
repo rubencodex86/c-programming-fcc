57dc9bf502d5dd0ef5b0fe21c91efb12dc57d23a
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 15:47:01 +0000

    Boolean

---
+++ Bool/Bool/Program.cs
@@ -0,0 +1,25 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Bool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value = true;
+
+            bool isMale = true;
+            Console.WriteLine(isMale);
+
+            isMale = false;
+            Console.WriteLine(isMale);
+
+            // mainly used to making decisions
+        }
+    }
+}
+
