042615620cee486f3316df318b9a794da9370807
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 13:48:26 +0000

    Text based data types

---
+++ String_Chars/String_Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba"; // " " empty
+            char letter = 'a';  // '\0' empty
+
+            Console.Write("Your name is: ");
+            Console.Write(name);
+            Console.WriteLine();
+            Console.WriteLine(letter);
+        }
+    }
+}
