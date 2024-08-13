f301497e1e44191cf6a2f75bb86d40563cfc8b92
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 18:21:02 +0000

    Const keyword

---
+++ const/const/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // to stop variable property
+            const int vat = 20;
+
+            //vat = 10;
+
+
+            int balance = 1000;
+
+            Console.WriteLine(balance * (vat/100D));
+
+            const double percentVAT = vat / 100D;
+            Console.WriteLine(balance * percentVAT);
+            
+            const string version = "v1.0";
+            
+        }
+    }
+}
