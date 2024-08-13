597dcc7812c515abe1fec5825af50247d8d1174c
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 18:12:00 +0000

    Var keyword

---
+++ var/var/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // the compiler will figure out what type it needs to based on the value initialized
+            // only use var when its very obvious ex... ages.
+            var age = 23;
+            //int age = -23;
+            Console.WriteLine(age);
+
+            var bigNumber = 900000000L;
+            //long bigNumber = 900000000L;
+            Console.WriteLine(bigNumber);
+
+            var negative = -55.3;
+            //double negative = -55.3D;
+            Console.WriteLine(negative);
+ 
+            var precision = 5.000001F;
+            //float precision = 5.000001F;
+            Console.WriteLine(precision);
+
+            var money = 14.99M;
+            //decimal money = 14.99M;
+            Console.WriteLine(money);
+
+            var name = "Aba"; 
+            var letter = 'a';
+        }
+    }
+}
