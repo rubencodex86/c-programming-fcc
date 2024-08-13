955b13b9e8641635e06c270ee58545e33a83a5a3
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-13 14:45:27 +0000

    Convert String to Number

---
+++ StringtoNumbers/StringtoNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringToNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textAge = "-23";
+            int age = Convert.ToInt32(textAge); // int is by definition int32
+            Console.WriteLine(age);
+
+            string textBigNumber = "900000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
+
+            string textNegative = "-55.3";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+
+            string textPrecision = "5.000001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
+
+            string textMoney = "14.99";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
+        }
+    }
+}
