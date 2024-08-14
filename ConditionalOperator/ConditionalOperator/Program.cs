722426cd4c3cf600bc9ca8d7ba7d4411ca9de004
rubencodex86 <133975993+rubencodex86@users.noreply.github.com>
2024-08-14 15:24:38 +0000

    Conditional operator

---
+++ ConditionalOperator/ConditionalOperator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int age = -10;
+            
+            /*
+            if (age >= 0)
+            {
+                Console.WriteLine("valid!");
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+            */
+            
+            //condition ? true : false
+
+            //string result = age >= 0 ? "Valid" : "Invalid";
+            //Console.WriteLine(result);
+            
+            //or
+
+            Console.WriteLine(age >= 0 ? "Valid" : "Invalid");
+
+        }
+    }
+}
